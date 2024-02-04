36dbf9539f2fd83e51a464a9ea263a5f144ad2d1
Thomas <152648240+Platundi@users.noreply.github.com>
2024-02-04 17:17:30 +0100

    Make grid dynamic Grid generation can now be done dynamically by a b utton. Color of fields is now random.

--- script.js
+++ script.js
@@ -1,27 +1,61 @@
-let i = 0;
-do {
-  let div = document.createElement("div");
-  div.classList.add("field");
-  document.querySelector(".container").appendChild(div);
-  i++;
-} while (i < 256);
+createGrid();
 
-let elements = document.querySelectorAll(".container .field");
-elements.forEach((element) => {
-  element.addEventListener("mouseenter", (event) => {
-    let hoveredElement = event.target;
-    hoveredElement.classList.add("hover");
+function createGrid(size = 16) {
+  let i = 0;
+  do {
+    let div = document.createElement("div");
+    div.classList.add("field");
+    div.style.flex = `0 0 ${(1 / size) * 100}%`;
+    document.querySelector(".container").appendChild(div);
+    i++;
+  } while (i < size ** 2);
+  hoverHandler();
+}
+
+function hoverHandler() {
+  let elements = document.querySelectorAll(".container .field");
+  elements.forEach((element) => {
+    element.addEventListener("mouseenter", (event) => {
+      let randomRed = Math.floor(Math.random() * 256);
+      let randomGreen = Math.floor(Math.random() * 256);
+      let randomBlue = Math.floor(Math.random() * 256);
+      let hoveredElement = event.target;
+      hoveredElement.style.backgroundColor =
+        "rgb(" + randomRed + "," + randomGreen + "," + randomBlue + ")";
+      // hoveredElement.classList.add("hover");
+    });
+    element.addEventListener("touchstart", (event) => {
+      let hoveredElement = event.target;
+      hoveredElement.classList.add("hover");
+    });
+    element.addEventListener("touchend", (event) => {
+      let hoveredElement = event.target;
+      hoveredElement.classList.add("hover");
+    });
   });
-  element.addEventListener("touchstart", (event) => {
-    let hoveredElement = event.target;
-    hoveredElement.classList.add("hover");
-  });
-  element.addEventListener("touchend", (event) => {
-    let hoveredElement = event.target;
-    hoveredElement.classList.add("hover");
-  });
+}
+
+let btn = document.querySelector(".btn-container");
+btn.addEventListener("click", () => {
+  let gridSize = 0;
+  while (true) {
+    gridSize = parseFloat(
+      prompt("Rastergröße eingeben. Nicht größer als 100!")
+    );
+
+    // Check if the input is a number and greater than 100
+    if (gridSize === null || isNaN(gridSize) || parseInt(gridSize) <= 100) {
+      break;
+    } else {
+      alert("Nicht größer als 100!");
+    }
+  }
+  let clearDiv = document.querySelector(".container");
+  while (clearDiv.firstChild) {
+    clearDiv.removeChild(clearDiv.firstChild);
+  }
+  createGrid(gridSize);
 });
-
 // let container = document.querySelector("div");
 // container.addEventListener("mouseenter", () => {
 //   container.classList.add("hover");
